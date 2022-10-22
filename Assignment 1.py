c0060a6263041b1c8be8cb54fceb1289b051b8ae
DeniseIra02 <irajubilo@gmail.com>
2022-10-22 15:51:47 +0800

    Added codes to display last letter. (letter H)

--- Assignment 1.py
+++ Assignment 1.py
@@ -2,8 +2,9 @@
 
 for row in range (7):
     for col in range (17):
-        if ((col==0) or ((row==0 or row==3) and (col>0 and col<4)) or ((col==4) and (row!=0 and row!=3)) or \
-            ((row==0 or row==3) and (col>6 and col<10)) or ((col==6 or col==10) and (row!=0))):
+        if ((col==0) or ((row==0 or row==3) and (col>0 and col<4)) or ((col==4) and (row!=0 and row!=3)) or 
+            ((row==0 or row==3) and (col>6 and col<10)) or ((col==6 or col==10) and (row!=0)) or 
+            ((col== 12 or col==15)) or (row==3 and (col>12 and col<15))):
             
             print("*", end=" ")
             
